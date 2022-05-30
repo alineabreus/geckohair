57d487d8eaf88ffa2b98efaeac237ea0e3f1be72
alineabreus <alineabreu1998@outlook.com>
2022-05-30 16:43:25 -0300

    Adicionando template front-end gratuito feito por: Colorlib

---
+++ resources/views/layouts/menu-sidebar.blade.php
@@ -0,0 +1,66 @@
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="images/icon/logo.png" alt="Cool Admin" />
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li class="active has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>Vendas</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="index.html">Dashboard 1</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Dashboard 2</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Dashboard 3</a>
+                        </li>
+                        <li>
+                            <a href="index4.html">Dashboard 4</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="chart.html">
+                        <i class="fas fa-chart-bar"></i>Categorias</a>
+                </li>
+                <li>
+                    <a href="table.html">
+                        <i class="fas fa-table"></i>Produtos</a>
+                </li>
+                <li>
+                    <a href="form.html">
+                        <i class="far fa-check-square"></i>Clientes</a>
+                </li>
+                <li>
+                    <a href="calendar.html">
+                        <i class="fas fa-calendar-alt"></i>Funcionários</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-handshake"></i>Serviços</a>
+                </li>
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-copy"></i>Agenda</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="login.html">Login</a>
+                        </li>
+                        <li>
+                            <a href="register.html">Register</a>
+                        </li>
+                        <li>
+                            <a href="forget-pass.html">Forget Password</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,43 @@
+<div class="account-wrap">
+    <div class="account-item clearfix js-item-menu">
+        <div class="image">
+            <img src="images/icon/avatar-02.jpg" alt="Aline Abreu" />
+        </div>
+        <div class="content">
+            <a class="js-acc-btn" href="#">Aline Abreu</a>
+        </div>
+        <div class="account-dropdown js-dropdown">
+            <div class="info clearfix">
+                <div class="image">
+                    <a href="#">
+                        <img src="images/icon/avatar-02.jpg" alt="Aline Abreu" />
+                    </a>
+                </div>
+                <div class="content">
+                    <h5 class="name">
+                        <a href="#">Aline Abreu</a>
+                    </h5>
+                    <span class="email">alineabreu@example.com</span>
+                </div>
+            </div>
+            <div class="account-dropdown__body">
+                <div class="account-dropdown__item">
+                    <a href="#">
+                        <i class="zmdi zmdi-account"></i>Account</a>
+                </div>
+                <div class="account-dropdown__item">
+                    <a href="#">
+                        <i class="zmdi zmdi-settings"></i>Setting</a>
+                </div>
+                <div class="account-dropdown__item">
+                    <a href="#">
+                        <i class="zmdi zmdi-money-box"></i>Billing</a>
+                </div>
+            </div>
+            <div class="account-dropdown__footer">
+                <a href="#">
+                    <i class="zmdi zmdi-power"></i>Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
